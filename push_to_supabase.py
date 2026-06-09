8dcb66af32e8487d351dbd2ae17910ba3e1a014b
vandissa-bren <bt.renfrey@vandissacapital.com>
2026-06-09 11:59:04 +1000

    Update push_to_supabase.py

--- push_to_supabase.py
+++ push_to_supabase.py
@@ -54,6 +54,7 @@ PBP_SLUG_MAP: dict[int, str] = {
     1664: "TheRallyPickleball",
     1714: "RunwayPickleball",
     1733: "pickleballpowerhouse"
+    1770: "rayapickleballclub",
 }
 
 VENUE_NAMES: dict[int, str] = {
@@ -74,6 +75,7 @@ VENUE_NAMES: dict[int, str] = {
     1664: "The Rally Pickleball | Altona",
     1714: "Runway Pickleball",
     1733: "Pickleball Powerhouse"
+    1770: "Raya Pickleball Club",
 }
 
 
