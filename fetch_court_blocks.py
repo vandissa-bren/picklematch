96a7a2cbb2e276d603a11e1ea027cfa05146f63c
vandissa-bren <bt.renfrey@vandissacapital.com>
2026-06-13 20:15:13 +1000

    Update fetch_court_blocks.py

--- fetch_court_blocks.py
+++ fetch_court_blocks.py
@@ -31,7 +31,8 @@ PBP_SLUG_MAP = {
     1664: "TheRallyPickleball",
     1714: "RunwayPickleball",
     1733: "pickleballpowerhouse",
-    1770: "rayapickleballclub"
+    1770: "rayapickleballclub",
+    1783: "PICKLE4REAL"
 }
 
 VENUE_NAMES = {
@@ -52,7 +53,8 @@ VENUE_NAMES = {
     1664: "The Rally Pickleball | Altona",
     1714: "Runway Pickleball",
     1733: "Pickleball Powerhouse",
-    1770: "Raya Pickleball Club"
+    1770: "Raya Pickleball Club",
+    1783: "Pickle4Real"
 }
 
 # Venues that use non-pickleball surface names for court hire
